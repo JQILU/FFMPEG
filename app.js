15713bf4ca71552fe54f1762076a78f4a4d39fd4
千年小小黑 <44860947+JQILU@users.noreply.github.com>
2020-04-07 19:04:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 const Koa = require('koa');
 const app = new Koa();
 const audioMixing = require('./audioMixing')
-const fs = require('fs');
 
 app.use(async ctx => {
     let path = ctx.path;
-    if (path === '/musicGenerate') {
-        await audioMixing.generateMusic(ctx.query).then(data => {
-            ctx.status = 200
-            ctx.body = {
-                codeMsg: 'success',
-                data: data
-            }
-        }).catch(data => {
-            ctx.status = 200
-            ctx.body = {
-                codeMsg: 'error',
-                data: data
-            }
-        })
-    }
+  
     
-    if (path === '/audioMix') {
-        await audioMixing.mixAudio(ctx.query).then(data => {
+    if (path === '/audioMixing') {
+        await audioMixing.main(ctx.query).then(data => {
             ctx.status = 200
             ctx.body = {
                 codeMsg: 'success',
